38577653e608850e8a8096835524b2a80db5b277
Ratul Islam <iratul010@gmail.com>
2024-10-31 15:02:45 +0600

    slidebar-done

--- src/Components/Slidebar/Slidebar.tsx
+++ src/Components/Slidebar/Slidebar.tsx
@@ -1,24 +1,76 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { LuArrowRightFromLine } from "react-icons/lu";
+import { LuArrowLeftFromLine } from "react-icons/lu";
+import { IoHomeOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi2";
+import { GiSkills } from "react-icons/gi";
+import { MdContactPhone } from "react-icons/md";
+import { GoFileSubmodule } from "react-icons/go";
+import { BsMoonStarsFill } from "react-icons/bs";
+import { AiFillSun } from "react-icons/ai";
+interface MenuItem {
+  path: string;
+  icon: JSX.Element;
+}
 
 const Slidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isThemeToggle, setIsThemeToggle] = useState(false);
+  const menuItems:MenuItem[] = [
+    { path: "/", icon: <IoHomeOutline /> },
+    { path: "/about", icon: <HiOutlineUser /> },
+    { path: "/skills", icon: <GiSkills /> },
+    { path: "/projects", icon: <GoFileSubmodule /> },
+    { path: "/contacts", icon: <MdContactPhone /> },
+  ];
   const toggleSlidebar = () => {
     setIsOpen(!isOpen);
   };
-  const menuItems = ["/", "/about", "/contact", "/projects", "/resume"];
+  const toggleTheme = () => {
+    const newThemeToggle = !isThemeToggle;
+    console.log(newThemeToggle)
+    setIsThemeToggle(newThemeToggle);
+    localStorage.setItem('themeToggle', newThemeToggle.toString()); 
+  };
+  
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('themeToggle');
+    if (savedTheme !== null) {
+      setIsThemeToggle(JSON.parse(savedTheme)); // Retrieve from localStorage
+    }
+  }, []);
+  
   return (
-    <div className={`${isOpen ? "slidebar__expanded" : ""} slidebar `}>
-      Slidebar
-      <button onClick={() => toggleSlidebar()}>increase</button>
-      <div className="slidebar__list">
-        {menuItems.map((path, index) => (
-          <Link key={index} to={path} className="slidebar__item">
-            {path === "/"
-              ? "Home"
-              : path.slice(1).charAt(0).toUpperCase()+path.slice(2)}
-          </Link>
-        ))}
+    <div className={` ${isOpen ? "slidebar__expanded" : ""} slidebar `}>
+      <div className="slidebar__container">
+        <button onClick={() => toggleSlidebar()} className="btn btn-slider">
+          {isOpen ? <LuArrowLeftFromLine /> : <LuArrowRightFromLine />}
+        </button>
+        <div className="slidebar__menus">
+          {menuItems.map((pathObj, index) => (
+            <Link key={index} to={pathObj.path} className="slidebar__item">
+              {isOpen ? (
+                pathObj.path === "/" ? (
+                  "Home"
+                ) : (
+                  pathObj.path.slice(1).charAt(0).toUpperCase() +
+                  pathObj.path.slice(2)
+                )
+              ) : (
+                <span className="slidebar__icons">{pathObj.icon}</span>
+              )}
+            </Link>
+          ))}
+        </div>
+        <div className="slidebar__setting">
+          <button
+            onClick={() => toggleTheme()}
+            className=" btn btn-slider slidebar__themeToggle"
+          >
+            {isThemeToggle ? <AiFillSun /> : <BsMoonStarsFill />}
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './Styles/main.scss';
 import AppRoutes from './Routes/AppRoutes'
+import './Styles/main.scss';
  
- 
-
 createRoot(document.getElementById('root')!).render(
 <StrictMode>
   <AppRoutes/>
